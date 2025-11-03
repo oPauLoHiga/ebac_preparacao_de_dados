5e2c024e656e537d525c11c05080ddad79260e84
Paulo Higa <paulojhiga@gmail.com>
2025-11-03 15:50:32 -0300

    Update intro_preparacao_dados.py

--- intro_preparacao_dados.py
+++ intro_preparacao_dados.py
@@ -1,4 +1,28 @@
 #Analise Exploratória de Dados (AED)
 import pandas as pd
 
-df = pd.read_csv('')
+df = pd.read_csv('clientes-v2.csv')
+
+print(df.head().to_string())
+print(df.tail().to_string())
+df['data'] = pd.to_datetime(df['data'], format='%d/%m/%Y', errors='coerce')
+
+print('Verificação: ')
+print(df.info())
+
+print('Analise de dados nulos:\n', df.isnull().sum())
+print('% de dados nulos:\n ',df.isnull.mean() * 100)
+df.dropna(inplace=True)
+print('Confirmar remoção de dados nulus:\n', df.isnull().sum())
+
+print('Analise de dados duplicados:\n', df.duplicated().sum())
+
+print('Analise de dados únicos:\n', df.nunique())
+
+print('Estatisticas dos dados:\n', df.describe())
+
+df = df[['idade','data','estado','salario','nivel-educacao',
+         'numero_filhos','estado_civel','area_atuacao']]
+print(df.head().to_string())
+
+df.to_csv('clientes-v2-tratados.csv', index=False)
