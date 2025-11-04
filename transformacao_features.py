41534dfa267bf08c3b2b22c1ed8f408bf24fcb2c
Paulo Higa <paulojhiga@gmail.com>
2025-11-04 10:25:44 -0300

    Update transformacao_features.py

--- transformacao_features.py
+++ transformacao_features.py
@@ -1,9 +1,24 @@
 import pandas as pd
 import numpy as np
 from scipy import stats
-
 pd.set_option('display.width', None)
-
 df = pd.read_csv('clientes-v2-tratados.csv')
 
 print(df.head())
+
+# Transformação Logaritmica
+df['salario_log'] = np.log1p(df['salario']) # log1p é usado para evitar problemas com valores zero
+print("\nDataFrame após transformação logaritmica no 'salario':\n", df.head())
+
+# Transformação Box-Cox
+df['salario_boxcox'], _ = stats.boxcox(df['salario'] + 1)
+print("\nDataFrame após transformação Box-Cox no 'salario':\n",df.head())
+
+# Codificação de Frequência para 'estado'
+estado_freq = df['estado'].value_counts() / len(df)
+df['estado_freq'] = df['estado'].map(estado_freq)
+print("\nDataFrame após codificação de frequência para 'estado':\n",df.head())
+
+# Interações
+df['interacao_idade_filhos'] = df['idade'] * df['numero_filhos']
+print("\nDataFrame após criação de interações entre 'idade' e 'numero_filhos':\n",df.head())
